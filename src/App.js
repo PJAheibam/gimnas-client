2d49ca1c60292a0304ece9ccc1b338d720f7e6ab
PJAheibam <prosenjitsingha28@gmail.com>
2022-09-28 23:28:58 +0600

    Header component created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import { ThemeProvider } from "styled-components";
 import { dark } from "./styles/Theme";
+import { GlobalStyle } from "./styles/GlobalStyles";
+import Header from "./components/header/Header";
 
 function App() {
-  return <ThemeProvider theme={dark}></ThemeProvider>;
+  return (
+    <ThemeProvider theme={dark}>
+      <GlobalStyle />
+      <Header />
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/styles/Wrapper.styles.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export default styled.div`
+  padding-inline: max(1.5rem, calc(100vw - 80rem));
+`;
